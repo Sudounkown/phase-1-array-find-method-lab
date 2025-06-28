a98706af1575f18f3a68c2e0b995307b207141e5
Roy Koigu <roykoigu@protonmail.com>
2025-06-28 11:56:45 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+// code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]
+
+let win //for some reason npmtest won't pass unless I declare this here 😅
+const superbowlWin = (array) => {
+    win = array.find(item => item["result"] === "W")
+    return win ? win.year : undefined
+
+}
+console.log(superbowlWin(record))
